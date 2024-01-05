1801837d94328830091452c1710bc3e01bca43e1
ussenuk <ukimanuka@gmail.com>
2024-01-05 18:35:32 +0300

    oxford_comma challenge solved

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return  ", ".join(items)
+    elif len(items) < 3:
+        items[-1] = "and " + items[-1]
+        return  " ".join(items)
+     
+    items[-1] = "and " + items[-1]
+    oxford_string = ", ".join(items)
+    return oxford_string  
